e2cdec29f140c11e4f1f811306ecff7c3f3fc33c
kyleyjackson <kjackson@bayviewglen.ca>
2023-05-30 11:31:34 -0400

    stuff pt 1

--- scripts/main.js
+++ scripts/main.js
@@ -4,33 +4,54 @@ const displacement = document.getElementById('d-st');
 const time = document.getElementById('t-st');
 const velocity = document.getElementById('v-st');
 const acceleration = document.getElementById('d-st');
+const goBtn = document.getElementById('go-btn');
+const resetBtn = document.getElementById('reset-btn');
 
 const dVal = document.getElementById
 
 //* numerical values
 const imgOffset = elem.offsetTop; //find y-pos of image in order to calculate displacement
-var displaceY = 0;
-var maxY = 0;
+const maxY = 690 + imgOffset; //*864px 
+console.log(maxY);
 
+//* booleans
+var cancelled = false;
+resetBtn.disabled = true;
 
 
 function myMove() {   
     var pos = imgOffset; //defaukt position  of image (subject)
     var id = null;
 
-    maxY = imgOffset + parseInt();
-
     clearInterval(id);
     id = setInterval(frame, 10);
 
     function frame() {
-        if (pos == maxY) {
+        if (cancelled === true) {
             clearInterval(id);
+            cancelled = false;
+            elem.style.top = imgOffset + 'px';
+            goBtn.disabled = false;
+            resetBtn.disabled = true;
+        } else if (pos == maxY) {
+            clearInterval(id);
+
+            goBtn.disabled = false;
         } else {
             pos++; 
             elem.style.top = pos + 'px'; 
-            console.log(pos);
+            console.log(pos - imgOffset);
+            goBtn.disabled = true;
+            resetBtn.disabled = false;
         }
+    }
+}
+
+function cancel() {
+    if (cancelled) {
+        cancelled = false;
+    } else {
+        cancelled = true;
     }
 }
 
