7eae9050ae6a6d3870d7e75bb759adc97b456f9c
kyleyjackson <kjackson@bayviewglen.ca>
2023-05-30 13:40:31 -0400

    working on value calcs

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,24 @@
 //* HTML elements
-const elem = document.getElementById("sc-standing");
+
+//value trackers
 const displacement = document.getElementById('d-st');
 const time = document.getElementById('t-st');
 const velocity = document.getElementById('v-st');
 const acceleration = document.getElementById('d-st');
+
+//buttons
 const goBtn = document.getElementById('go-btn');
 const resetBtn = document.getElementById('reset-btn');
 
-const dVal = document.getElementById
+//input fields
+const gCheck = document.getElementById('g-check');
+const arCheck = document.getElementById('ar-check');
+const arVal = document.getElementById('ar-val');
+
+//misc
+const elem = document.getElementById("sc-standing");
+
+
 
 //* numerical values
 const imgOffset = elem.offsetTop; //find y-pos of image in order to calculate displacement
@@ -17,32 +28,58 @@ console.log(maxY);
 //* booleans
 var cancelled = false;
 resetBtn.disabled = true;
+resetBtn.classList.add('is-hovered');
 
 
 function myMove() {   
-    var pos = imgOffset; //defaukt position  of image (subject)
+    var pos = imgOffset; //default position  of image (subject)
     var id = null;
 
     clearInterval(id);
-    id = setInterval(frame, 10);
+    id = setInterval(frame, 2);
 
     function frame() {
         if (cancelled === true) {
+            //image modifiers
             clearInterval(id);
-            cancelled = false;
             elem.style.top = imgOffset + 'px';
+
+            //buttons
             goBtn.disabled = false;
+            goBtn.classList.remove('is-hovered');
             resetBtn.disabled = true;
+            resetBtn.classList.add('is-hovered');
+            
+            //misc
+            cancelled = false;
         } else if (pos == maxY) {
+            //image modifiers
             clearInterval(id);
 
+            //buttons
             goBtn.disabled = false;
+            goBtn.classList.remove('is-hovered');
+
+            //console logs
+            console.log(pos === maxY);
         } else {
+            //image modifiers
             pos++; 
             elem.style.top = pos + 'px'; 
-            console.log(pos - imgOffset);
+
+            //value modifiers
+            displacement.textContent = 
+            velocity.textContent = 
+
+
+            //buttons
             goBtn.disabled = true;
+            goBtn.classList.add('is-hovered');
             resetBtn.disabled = false;
+            resetBtn.classList.remove('is-hovered');
+
+            //console logs
+            console.log(pos - imgOffset);
         }
     }
 }
@@ -63,3 +100,9 @@ document.onmousemove = function(e) {
 
     //!console.log(txt);
 }
+
+//kinematic equations
+function vf(vi, a, t) {
+    return vi + (a * t);
+}
+
